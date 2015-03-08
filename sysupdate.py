6dc16be106dd031a50d91df257daf5bc01c1a9b2
vnn <vnn@yasin.io>
2015-03-08 09:07:35 +0100

    pep8 valid + change script name

--- sysupdate.py
+++ sysupdate.py
@@ -4,6 +4,7 @@
 Manage OpenBSD-stable userland, ports and kernel updates.
 """
 
+__version__ = '0.1'
 
 import os
 import subprocess
@@ -49,18 +50,22 @@ def build():
 
 
 if __name__ == "__main__":
-    # Arguments parsing.
-    parser = argparse.ArgumentParser(description=('Manage OpenBSD-stable userland, '
-                                                  'ports and kernel updates.'),
-                                     prog='obsdupdate')
+    # Parse arguments.
+    parser = argparse.ArgumentParser(
+        description=('Manage OpenBSD-stable userland, '
+                     'ports and kernel updates.'), prog='sysupdate')
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-f', '--fetch', choices=['ports', 'src', 'xenocara'],
+    group.add_argument('-f', '--fetch',
+                       choices=['ports', 'src', 'xenocara'],
                        help='Fetch sources from CVS.')
-    group.add_argument('-u', '--update', choices=['ports', 'src', 'xenocara'],
+    group.add_argument('-u', '--update',
+                       choices=['ports', 'src', 'xenocara'],
                        help='Update source tree from CVS.')
-    group.add_argument('-b', '--build', choices=['kernel', 'userland', 'xenocara'],
+    group.add_argument('-b', '--build',
+                       choices=['kernel', 'userland', 'xenocara'],
                        help='Rebuild and install a part of the system.')
-    parser.add_argument('-c', '--cvs', nargs=1, metavar='CVSROOT',
+    parser.add_argument('-c', '--cvs',
+                        nargs=1, metavar='CVSROOT',
                         help='Define CVS root server.')
     args = parser.parse_args()
 
@@ -69,14 +74,14 @@ if __name__ == "__main__":
         print('Error: this command should be run as root')
         exit()
 
-    # Configuration variables.
+    # Define configuration variables.
     default_cvs_root = 'anoncvs@anoncvs.fr.openbsd.org:/cvs'
     cvsroot = args.cvs if args.cvs else default_cvs_root
     release = 'OPENBSD_' + os.uname()[2].replace('.', '_')
-    repo = [vars(args)[i] for i in ['fetch','update','build'] if vars(args)[i]][0]
+    repo = [vars(args)[i] for i in ['fetch', 'update', 'build'] if vars(args)[i]][0]
     arch = os.uname()[4]
 
-    # Arguments processing.
+    # Process arguments.
     if args.fetch:
         fetch()
     elif args.update:
