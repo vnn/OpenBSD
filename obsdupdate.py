2335c96bf2f8f1848369114da2d857fbb1b2ef11
vnn <vnn@yasin.io>
2015-03-08 06:20:24 +0100

    better variables names to fit the advices of my code-fu master @tamentis

--- obsdupdate.py
+++ obsdupdate.py
@@ -13,9 +13,9 @@ Usage:
 Options:
   -h --help        Show this screen.
   -v --version     Show version.
-  -f --fetch       Fetch sources onto the system.
-  -u --update      Update source tree.
-  -b --build       Rebuild the system.
+  -f --fetch       Fetch sources from CVS.
+  -u --update      Update source tree from CVS.
+  -b --build       Rebuild and install a part of the system.
   -c --cvs         Define anonymous CVS root server.
 """
 
@@ -32,25 +32,25 @@ def cmd(cmd):
 
 def fetch():
 
-    """ Fetch specified part of the system. """
+    """ Fetch corresponding sources from CVS. """
 
-    print('Fetching OpenBSD '+part+' tree from CVS...')
-    cmd('cd /usr && cvs -qd '+cvsroot+' get -r'+release+' -P '+part)
+    print('Fetching OpenBSD '+repo+' tree from CVS...')
+    cmd('cd /usr && cvs -qd '+cvsroot+' get -r'+release+' -P '+repo)
 
 
 def update():
 
-    """ Update specified part of the system. """
+    """ Update corresponding sources from CVS. """
 
-    print('Updating OpenBSD '+part+' tree from CVS...')
-    cmd('cd /usr/'+part+' && cvs -d '+cvsroot+' -q up -r'+release + ' -Pd')
+    print('Updating OpenBSD '+repo+' tree from CVS...')
+    cmd('cd /usr/'+repo+' && cvs -d '+cvsroot+' -q up -r'+release + ' -Pd')
 
 
 def build():
 
-    """ Build specified part of the system. """
+    """ Build and install corresponding part of the system. """
 
-    print('Rebuilding '+part+'...')
+    print('Rebuilding '+repo+'...')
     if args['kernel']:
         cmd('cd /usr/src/sys/arch/'+arch+'/conf && config GENERIC.MP')
         cmd('cd /usr/src/sys/arch/'+arch+'/compile/GENERIC.MP '
@@ -68,11 +68,11 @@ if __name__ == "__main__":
     args = docopt.docopt(__doc__, version='obsdupdate 0.2.0')
 
     # Configuration variables goes here.
-    default_cvs = 'anoncvs@anoncvs.fr.openbsd.org:/cvs'
-    cvsroot = args['--cvs'] if args['--cvs'] else default_cvs
+    default_cvs_root = 'anoncvs@anoncvs.fr.openbsd.org:/cvs'
+    cvsroot = args['--cvs'] if args['--cvs'] else default_cvs_root
     release = 'OPENBSD_' + os.uname()[2].replace('.', '_')
+    repo = sys.argv[2]
     arch = os.uname()[4]
-    part = sys.argv[2]
 
     # Process arguments
     if args['--fetch']:
