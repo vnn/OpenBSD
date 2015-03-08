9ab14fbbf18a41bd219bf8d8efaddaa5b29f3107
vnn <vnn@yasin.io>
2015-03-08 07:52:20 +0100

    remove sys import, cleaner is better

--- obsdupdate.py
+++ obsdupdate.py
@@ -6,7 +6,6 @@ Manage OpenBSD userland, ports and kernel updates.
 
 
 import os
-import sys
 import subprocess
 import argparse
 
@@ -50,27 +49,29 @@ def build():
 
 
 if __name__ == "__main__":
+    # Arguments parsing.
     parser = argparse.ArgumentParser(description=('Manage OpenBSD userland, '
-                                                 'ports and kernel updates.'),
+                                                  'ports and kernel updates.'),
                                      prog='obsdupdate')
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-f', '--fetch', choices=['ports', 'src', 'xenocara'],
-                        help='Fetch sources from CVS.')
+                       help='Fetch sources from CVS.')
     group.add_argument('-u', '--update', choices=['ports', 'src', 'xenocara'],
-                        help='Update source tree from CVS.')
+                       help='Update source tree from CVS.')
     group.add_argument('-b', '--build', choices=['kernel', 'userland', 'xenocara'],
-                        help='Rebuild and install a part of the system.')
-    parser.add_argument('-c', '--cvs', nargs=1, help='Define CVS root server.')
+                       help='Rebuild and install a part of the system.')
+    parser.add_argument('-c', '--cvs', nargs=1, metavar='CVSROOT',
+                        help='Define CVS root server.')
     args = parser.parse_args()
 
-    # Configuration variables goes here.
+    # Configuration variables.
     default_cvs_root = 'anoncvs@anoncvs.fr.openbsd.org:/cvs'
     cvsroot = args.cvs if args.cvs else default_cvs_root
     release = 'OPENBSD_' + os.uname()[2].replace('.', '_')
-    repo = sys.argv[2]
+    repo = [vars(args)[i] for i in ['fetch','update','build'] if vars(args)[i]][0]
     arch = os.uname()[4]
 
-    # Process arguments
+    # Arguments processing.
     if args.fetch:
         fetch()
     elif args.update:
