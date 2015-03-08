ccbcfaef14496ccb465b945c9f6c1afc0c078c54
vnn <vnn@yasin.io>
2015-03-08 04:27:33 +0100

    trying to make things more clean

--- obsdupdate.py
+++ obsdupdate.py
@@ -26,47 +26,60 @@ import subprocess
 import docopt
 
 
-def main():
+def cmd(cmd):
+    subprocess.call(cmd, shell=True)
 
-    """
-    Fetch, update or build specified part of the system.
-    """
 
-    def cmd(cmd):
-        subprocess.call(cmd, shell=True)
+def fetch():
 
+    """ Fetch specified part of the system. """
+
+    print('Fetching OpenBSD '+part+' tree from CVS...')
+    cmd('cd /usr && cvs -qd '+cvsroot+' get -r'+release+' -P '+part)
+
+
+def update():
+
+    """ Update specified part of the system. """
+
+    print('Updating OpenBSD '+part+' tree from CVS...')
+    cmd('cd /usr/'+part+' && cvs -d '+cvsroot+' -q up -r'+release + ' -Pd')
+
+
+def build():
+
+    """ Build specified part of the system. """
+
+    print('Rebuilding '+part+'...')
+    if args['kernel']:
+        cmd('cd /usr/src/sys/arch/'+arch+'/conf && config GENERIC.MP')
+        cmd('cd /usr/src/sys/arch/'+arch+'/compile/GENERIC.MP '
+            '&& make clean && make && make install')
+    elif args['userland']:
+        cmd('rm -rf /usr/obj/* && cd /usr/src && make obj')
+        cmd('cd /usr/src/etc && env DESTDIR=/ make distrib-dirs')
+        cmd('cd /usr/src && make build')
+    elif args['xenocara']:
+        cmd('rm -rf /usr/xobj/*')
+        cmd('cd /usr/xenocara && make bootstrap && make obj && make build')
+
+
+if __name__ == "__main__":
+    args = docopt.docopt(__doc__, version='obsdupdate 0.2.0')
+
+    # Configuration variables goes here.
     default_cvs = 'anoncvs@anoncvs.fr.openbsd.org:/cvs'
     cvsroot = args['--cvs'] if args['--cvs'] else default_cvs
     release = 'OPENBSD_' + os.uname()[2].replace('.', '_')
     arch = os.uname()[4]
     part = sys.argv[2]
 
-    # Fetch
+    # Process arguments
     if args['--fetch']:
-        print('Fetching OpenBSD '+part+' tree from CVS...')
-        cmd('cd /usr && cvs -qd '+cvsroot+' get -r'+release+' -P '+part)
-    # Update
+        fetch()
     elif args['--update']:
-        print('Updating OpenBSD '+part+' tree from CVS...')
-        cmd('cd /usr/'+part+' && cvs -d '+cvsroot+' -q up -r'+release + ' -Pd')
-    # Build
+        update()
     elif args['--build']:
-        print('Rebuilding '+part+'...')
-        if args['kernel']:
-            cmd('cd /usr/src/sys/arch/'+arch+'/conf && config GENERIC.MP')
-            cmd('cd /usr/src/sys/arch/'+arch+'/compile/GENERIC.MP '
-                '&& make clean && make && make install')
-        elif args['userland']:
-            cmd('rm -rf /usr/obj/* && cd /usr/src && make obj')
-            cmd('cd /usr/src/etc && env DESTDIR=/ make distrib-dirs')
-            cmd('cd /usr/src && make build')
-        elif args['xenocara']:
-            cmd('rm -rf /usr/xobj/*')
-            cmd('cd /usr/xenocara && make bootstrap && make obj && make build')
+        build()
 
     print('done')
-
-
-if __name__ == "__main__":
-    args = docopt.docopt(__doc__, version='obsdupdate 0.2.0')
-    main()
