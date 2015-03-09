2d4a8aac39099def338d5bb61974a80b18a3b986
vnn <vnn@yasin.io>
2015-03-09 23:38:33 +0100

    polishing

--- bfcrypt.py
+++ bfcrypt.py
@@ -7,7 +7,6 @@ Encrypt or decrypt a file using Blowfish cipher.
 import argparse
 from getpass import getpass
 import os
-import sys
 
 from Crypto.Cipher import Blowfish
 from Crypto import Random
@@ -17,13 +16,13 @@ class BlowfishCipher:
 
     """ This class handles all the cryptograpgic operations. """
 
-    def __init__(self, password, iv=None):
+    def __init__(self, key, iv=None):
         self.__bs = bs
         self.__iv = iv if iv else Random.new().read(self.__bs)
-        self.__cipher = Blowfish.new(password, Blowfish.MODE_CBC, self.__iv)
+        self.__cipher = Blowfish.new(key, Blowfish.MODE_CBC, self.__iv)
 
     def __add_pad(self, chunk):
-        """ Add padding bytes. """
+        """ Return a chunk with padding added. """
         # The padding boundary is specified by the latest byte.
         # See: scheme ISO 10126
         padding = self.__bs - (len(chunk) % self.__bs)
@@ -32,11 +31,11 @@ class BlowfishCipher:
         return chunk
 
     def __del_pad(self, chunk):
-        """ Remove padding bytes. """
+        """ Return a chunk with padding removed. """
         return chunk[:-int(chunk[-1:])]
 
     def encrypt(self, chunk, first_part=False):
-        """ Encrypt chunk. """
+        """ Return an encrypted chunk. """
         # Prepend iv to the first chunk.
         if first_part and (len(chunk) != chunk_size):
             return self.__iv + self.__cipher.encrypt(self.__add_pad(chunk))
@@ -48,26 +47,26 @@ class BlowfishCipher:
             return self.__cipher.encrypt(chunk)
 
     def decrypt(self, chunk):
-        """ Decrypt chunk. """
+        """ Return a decrypted chunk. """
         if len(chunk) != chunk_size:
             return self.__del_pad(self.__cipher.decrypt(chunk))
         else:
             return self.__cipher.decrypt(chunk)
 
 
-def get_password():
-    """ This function handles password related operations. """
+def get_key():
+    """ Return a key grabbed from used input. """
     if args.encrypt:
         prompt = lambda: (getpass('Password: '),
                           getpass('Retype password: '))
-        password, password_verif = prompt()
-        while password != password_verif:
+        key, key_verif = prompt()
+        while key != key_verif:
             print('Passwords do not match. Try again')
-            password, password_verif = prompt()
+            key, key_verif = prompt()
     elif args.decrypt:
-        password = getpass()
+        key = getpass()
 
-    return password
+    return key
 
 
 if __name__ == "__main__":
@@ -82,33 +81,33 @@ if __name__ == "__main__":
                         metavar='<infile>', help='read data from <infile>')
     parser.add_argument('outfile', type=argparse.FileType('wb'),
                          metavar='<outfile>',
-                         help='write data to <outfile> (default: stdout)')
+                         help='write data to <outfile>')
     args = parser.parse_args()
 
     with open(args.infile.name, 'rb') as f:
-        with open(args.outfile.name, 'wb') as fp:
-            bs = Blowfish.block_size
-            chunk_size = 720
+        outfile = open(args.outfile.name, 'wb')
+        bs = Blowfish.block_size
+        chunk_size = 720
 
-            # Init the cipher using password from user input and iv.
-            if args.decrypt:
-                iv = f.read(bs)
-                bfcrypt = BlowfishCipher(get_password(), iv)
-            # Init the cipher using password from user input,
-            # and prepend iv to the first encrypted chunk.
-            elif args.encrypt:
-                chunk = f.read(chunk_size)
-                bfcrypt = BlowfishCipher(get_password())
-                fp.write(bfcrypt.encrypt(chunk, first_part=True))
+        # Init cipher for encryption using key from user input and iv.
+        if args.decrypt:
+            iv = f.read(bs)
+            bfcrypt = BlowfishCipher(get_key(), iv)
 
-            # Encrypt or decrypt the file chunk by chunk.
-            while True:
-                chunk = f.read(chunk_size)
-                if chunk and args.encrypt:
-                    fp.write(bfcrypt.encrypt(chunk))
-                elif chunk and args.decrypt:
-                    fp.write(bfcrypt.decrypt(chunk))
-                else:
-                    break
+        # Init cipher for decryption using key from user input,
+        # and prepend iv to the first encrypted chunk.
+        elif args.encrypt:
+            chunk = f.read(chunk_size)
+            bfcrypt = BlowfishCipher(get_key())
+            outfile.write(bfcrypt.encrypt(chunk, first_part=True))
 
-    print('done')
+        # Encrypt or decrypt the rest of the file chunk by chunk.
+        while True:
+            chunk = f.read(chunk_size)
+            if chunk and args.encrypt:
+                outfile.write(bfcrypt.encrypt(chunk))
+            elif chunk and args.decrypt:
+                outfile.write(bfcrypt.decrypt(chunk))
+            else:
+                print('done')
+                break
