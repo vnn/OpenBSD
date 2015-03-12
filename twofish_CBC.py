a75ded1d8e05971fa864bb6839f84b301f0c4dd2
vnn <vnn@yasin.io>
2015-03-12 05:05:14 +0100

    update, works

--- twofish_CBC.py
+++ twofish_CBC.py
@@ -12,64 +12,56 @@ import os
 
 from twofish import Twofish
 
-
 class TwofishCipherCBC:
 
-    """ This class handles all the cryptographic operations. """
+    """
+    This class handles all the cryptographic operations.
+    """
 
     def __init__(self, key, iv=None):
-        self.__bs = bs
-        self.__iv = iv if iv else os.urandom(self.__bs)
-        self.__prev = self.__iv  # To be used with XOR in CBC Mode
+        self.__block_size = block_size
+        self.__iv = iv if iv else os.urandom(self.__block_size)
+        self.__prev = bytes()
         self.__cipher = Twofish(key)
 
-    def encrypt(self, buffer, first_buffer=False):
-        """
-        Return an encrypted buffer.
-        """
-        if len(buffer) != bs:
-            self.__prev = self.__cipher.encrypt(self.__xor(self.__add_pad(buffer)))
-            return self.__prev
-        else:
-            self.__prev = self.__cipher.encrypt(self.__xor(buffer))
-            if first_buffer:
-                return self.__iv + self.__prev
+    def encrypt(self, buf, first_buf=False):
+        """ Return an encrypted block. """
+        if len(buf) == block_size:
+            if not self.__prev:
+                self.__prev = self.__iv
+                return self.__iv
             else:
+                self.__prev = self.__cipher.encrypt(self.__xor(buf, self.__prev))
                 return self.__prev
-
-    def decrypt(self, buffer, unpad=False):
-        """
-        Return a decrypted buffer.
-        """
-        if unpad:
-            return self.__del_pad(self.__xor(self.__cipher.decrypt(buffer)))
         else:
-            self.__prev = self.__xor(self.__cipher.decrypt(buffer))
+            self.__prev = self.__cipher.encrypt(self.__xor(self.__pad(buf), self.__prev))
             return self.__prev
 
-    def __add_pad(self, buffer):
-        """
-        Return a buffer with padding added (ISO 10126).
-        """
-        padding = self.__bs - len(buffer)
+    def decrypt(self, buf, unpad=False):
+        """ Return a decrypted block. """
+        if unpad:
+            return self.__unpad(self.__xor(self.__cipher.decrypt(buf), self.__prev))
+        else:
+            xored = self.__xor(self.__cipher.decrypt(buf), self.__prev)
+            self.__prev = buf
+            return xored
+
+    def __pad(self, buf):
+        """ Return a block with padding added (ISO 10126). """
+        padding = self.__block_size - len(buf)
         for idx, i in enumerate(range(padding), start=1):
-            buffer += os.urandom(1) if idx != padding else bytes([padding])
-        return buffer
+            buf += os.urandom(1) if idx != padding else bytes([padding])
+        return buf
 
-    def __del_pad(self, buffer):
-        """
-        Return a buffer with padding removed (ISO 10126).
-        """
-        return buffer[:-buffer[-1]]
+    def __unpad(self, buf):
+        """ Return a buf with padding removed (ISO 10126). """
+        return buf[:-buf[-1]]
 
-    def __xor(self, buffer):
-        """
-        XOR a buffer with the previous encrypted buffer,
-        or with the initializing vector (for the 1st buffer only).
-        """
-        buffer = int.from_bytes(buffer, byteorder='big')
-        prev_buffer = int.from_bytes(self.__prev, byteorder='big')
-        return (buffer ^ prev_buffer).to_bytes(self.__bs, byteorder='big')
+    def __xor(self, x, y):
+        """ Return the XOR from two 16bytes blocks. """
+        x = int.from_bytes(x, byteorder='big')
+        y = int.from_bytes(y, byteorder='big')
+        return (x ^ y).to_bytes(16, byteorder='big')
 
 
 def get_key():
@@ -104,33 +96,31 @@ if __name__ == "__main__":
     with open(args.infile.name, 'rb') as f:
         filesize = os.path.getsize(args.infile.name)
         outfile = open(args.outfile.name, 'wb')
-        bs = 16
+        block_size = 16
 
         # Get the initialization vector from the first 16 bytes
-        # of the file and use it with the key to initialize
-        # Twofish cipher.
+        # of the file and use it with the key to initialize Twofish.
         if args.decrypt:
-            iv = f.read(bs)
+            iv = f.read(block_size)
             cipher = TwofishCipherCBC(get_key(), iv)
-        # Initialize Twofish cipher with the key and write the iv
-        # + the first encrypted block to the outfile.
+        # Initialize Twofish cipher with the key provided
+        # by the user.
         elif args.encrypt:
-            buffer = f.read(bs)
             cipher = TwofishCipherCBC(get_key())
-            outfile.write(cipher.encrypt(buffer, first_buffer=True))
 
         # Process the rest of the file chunk by chunk.
         # When decrypting, wait for the last block, then remove
         # previously added padding.
         while True:
-            buffer = f.read(bs)
-            if buffer and args.encrypt:
-                outfile.write(cipher.encrypt(buffer))
-            elif buffer and args.decrypt:
+            buf = f.read(block_size)
+
+            if buf and args.encrypt:
+                outfile.write(cipher.encrypt(buf))
+            elif buf and args.decrypt:
                 if f.tell() == filesize:
-                    outfile.write(cipher.decrypt(buffer, unpad=True))
+                    outfile.write(cipher.decrypt(buf, unpad=True))
                 else:
-                    outfile.write(cipher.decrypt(buffer))
+                    outfile.write(cipher.decrypt(buf))
             else:
                 print('done')
                 break
